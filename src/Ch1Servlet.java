2188d975528997d0e523b3a92f2562d1241ec8eb
jiangqiaonan <jiangqiaonan@renrenche.com>
2020-08-13 08:04:23 +0800

    feat:hello world

---
+++ src/Ch1Servlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+/**
+ *
+ */
+public class Ch1Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        Date today = new Date();
+        writer.println("<html><body><h1 align=center>HF\'s Chapter1 Servlet</h1><br>" + today + "</body></html>");
+    }
+}
